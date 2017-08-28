3b4714d7247b86742d7fde6f1e69b0726716e14d
Sascha Wessel <swessel@gr4yweb.de>
2017-08-28 13:06:12 +0200

    moved lib/config to lib/config.example.php and added lib/config to .gitignore

--- lib/config.example.php
+++ lib/config.example.php
@@ -8,7 +8,8 @@ class Config {
     'host' => '127.0.0.1',
     'name' => 'dashboard',
     'username' => 'dashboard',
-    'password' => 'p4ssw0rd'
+    'password' => '7xv1elEAZ6bkZeDh',
+    'driver_options' => [\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8']
   );
 
 }
